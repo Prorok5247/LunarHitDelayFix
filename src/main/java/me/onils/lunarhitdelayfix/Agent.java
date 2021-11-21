61227a7e136becc301e3305dc92b631209d47f89
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-21 18:23:03 +0100

    Removed debug print

--- src/main/java/me/onils/lunarhitdelayfix/Agent.java
+++ src/main/java/me/onils/lunarhitdelayfix/Agent.java
@@ -4,7 +4,6 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String args, Instrumentation inst){
-        System.err.println("Inject started");
         inst.addTransformer(new Transformer(), true);
     }
 }
